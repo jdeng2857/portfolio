4e880537380d476cdccb62cdb519be4643431a29
jdeng2857 <jdeng2857@gmail.com>
2023-07-04 19:50:46 -0400

    project cards

--- src/app/components/ProjectCard.js
+++ src/app/components/ProjectCard.js
@@ -7,7 +7,7 @@ export const ProjectCard = ({ title, description, imgUrl }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-        <Image src={imgUrl} alt={title}/>
+        <Image className="project-image" src={imgUrl} alt={title}/>
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -2,12 +2,13 @@
 
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
+import portfolioImg from "../assets/img/portfolio.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import Image from "next/image";
 
 export const Projects = () => {
 
@@ -15,7 +16,7 @@ export const Projects = () => {
     {
       title: "Portfolio Site",
       description: "The site you're on. Made with React.js and deployed on Vercel",
-      imgUrl: projImg1,
+      imgUrl: portfolioImg,
     },
     {
       title: "Bird Classifier",
@@ -38,17 +39,17 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p></p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    <Nav.Item className="project-tab">
+                      <Nav.Link eventKey="first">Projects</Nav.Link>
                     </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    <Nav.Item className="project-tab">
+                      <Nav.Link eventKey="second">Learning</Nav.Link>
                     </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    <Nav.Item className="project-tab">
+                      <Nav.Link eventKey="third">Other Experience</Nav.Link>
                     </Nav.Item>
                   </Nav>
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
@@ -79,7 +80,6 @@ export const Projects = () => {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
     </section>
   )
 }
